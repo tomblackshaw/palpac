e4843d191a3fe777d35d1811905ce3203ed3860d
Tom Blackshaw <tom.blackshaw@protonmail.com>
2024-05-21 07:30:04 +0200

    added my.classes.__init__

---
+++ src/test_my/classes/__init__.py
@@ -0,0 +1,23 @@
+'''
+Created on May 21, 2024
+
+@author: Tom Blackshaw
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
