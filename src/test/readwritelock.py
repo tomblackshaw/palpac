fe1131674bd887157b2fd69ef7a2f1637b1c6876
Tom Blackshaw <tom.blackshaw@protonmail.com>
2024-05-23 18:12:54 +0200

    Added dummy testfile for readwritelock

---
+++ src/test/readwritelock.py
@@ -0,0 +1,45 @@
+'''
+Created on May 23, 2024
+
+@author: Tom Blackshaw
+'''
+import unittest
+# from my.classes import ReadWriteLock
+
+# TODO: Write a comprehensive test of ReadWriteLock
+
+# class SimpleClass:
+#
+#     def __init__(self):
+#         self.myval = None
+#
+#     @property
+#     def myval(self):
+#         return self.myval
+#
+#     @myval.setter
+#     def myval(self, x):
+#         self.myval = x
+#
+#     @myval.deleter
+#     def myval(self):
+#         del self.myval
+
+# class Test(unittest.TestCase):
+#
+#     def setUp(self):
+#         pass
+#
+#     def tearDown(self):
+#         pass
+#
+#     def testReadAcquireAndRelease(self):
+#         lck = ReadWriteLock()
+#         for i in range(0,10):
+#             lck.acquire_read()
+#             for i in range(0,10):
+#                 lck.release_read()
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.testReadWriteLock1']
+    unittest.main()
