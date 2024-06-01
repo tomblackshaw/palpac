6f327a11d26c825852ed453a5a88fcd795cfb362
Tom Blackshaw <tom.blackshaw@protonmail.com>
2024-06-01 14:04:44 +0200

    Sheepdip in progress

--- src/sheepdip.py
+++ src/sheepdip.py
@@ -9,17 +9,27 @@ Created on May 21, 2024
 
 EXAMPLE
 
-from sheepdip import *
 from ast import literal_eval
 from my.speechrecognition import SpeechRecognitionSingleton as s2t
+s2t.always_adjust = True
+s2t.max_recording_time = 3
+s2t.pause_threshold = 0.8
 
-See https://realpython.com/python-speech-recognition/
-google_text =s2t.recognizer.recognize_google(audio_data)
-dct = s2t.recognizer.recognize_google(audio_data, show_all=True)
-            text_queue.put((start_datestamp, finish_datestamp, interpause_in_microseconds, text))
+mic = s2t.microphone
 
+mic = s2t.api.Microphone()
+with mic as source:
+    recog = s2t.api.Recognizer()
+#    recog.adjust_for_ambient_noise(source)
+    audio =recog.listen(source)
+s2t.recognize(audio)
 
-
+from sheepdip import *
+audio = s2t.listen()
+s2t.recognize(audio)
+from my.text2speech import Text2SpeechSingleton as tts
+tts.voice = 'Freya'
+tts.say("In the beginning was the word, and the word was with God, and the word was God.")
 """
 
 from _queue import Empty
@@ -29,16 +39,17 @@ import datetime
 import os
 import sys
 import time
+from speech_recognition import AudioSource
 
 from my.classes.exceptions import StillAwaitingCachedValue, MutedMicrophoneError, UnknownCommandError, MicrophoneTimeoutError
 from my.classes.selfcachingcall import SelfCachingCall
 from my.stringutils import find_trigger_phrase_in_sentence, scan_sentence_for_any_one_of_these_trigger_phrases, generate_triggerphrase_permutations, \
-    trim_away_the_trigger_and_locate_the_command_if_there_is_one
+    trim_away_the_trigger_and_locate_the_command_if_there_is_one, text2time
 from my.tools import logit
 from my.weather import generate_weather_audio
 
-words_that_sound_like_hey = 'either hate hey hay they a heh eight i Freya up great the there edit'
-words_that_sound_like_dad = 'doc Dad there that tad thad than Dan dove dog'
+words_that_sound_like_hey = 'he uh oh hayden either hate hey hay they a heh eight i Freya up great the there edit'
+words_that_sound_like_dad = 'doc Dad there that tad thad than Dan dove dog the'
 G_trigger_phrases = words_that_sound_like_dad.split(' ') + generate_triggerphrase_permutations(
     words_that_sound_like_hey.split(' '),
     words_that_sound_like_dad.split(' '))
@@ -59,7 +70,10 @@ def process_text(tts, text):
 #    print("voice =", tts.voice)
     if 'marco' in text:
         print("POLO")
-    if 'weather' in text:
+    elif 'alarm' in text:
+        print("ALARM")
+        handle_alarm_command(tts, text)
+    elif 'weather' in text:
         print("WEATHER")
         try:
             tts.play(G_our_cached_weather_report_messages.result['shorter audio'])
@@ -138,11 +152,11 @@ def indefinitely_turn_text_into_commands(s2t, text_queue, triggerphrases, goadin
     datestamp_of_last_goading = datetime.datetime.now()
     while not G_stop:
         if demand_triggerphrase is False and (datetime.datetime.now() - datestamp_of_last_goading).total_seconds() > max_secs_for_goading:
-            print("Your no-trigger window has closed.")
+#            print("Your no-trigger window has closed.")
             demand_triggerphrase = True
         time.sleep(.1)
         if sentence.count(' ') > max_words_in_cache:
-            print("Truncating ...")
+#            print("Truncating ...")
             sentence = sentence[sentence.index(' ') + 1:]
         sentence = (sentence + ' ' + get_all_from_queue(text_queue)).strip()
         if len(sentence) == 0:
@@ -150,7 +164,8 @@ def indefinitely_turn_text_into_commands(s2t, text_queue, triggerphrases, goadin
         else:
             cutoff_point = trim_away_the_trigger_and_locate_the_command_if_there_is_one(sentence, triggerphrases)
             if cutoff_point >= len(sentence):
-#                 print(">>{sentence}<< After removing the trigger phrase(s), I find no command.".format(sentence=sentence))
+#                print(">>{sentence}<< After removing the trigger phrase(s), I find no command.".format(sentence=sentence))
+                print("Waiting for follow-up command to be spoken...")
                 goading_call()  # So, goad them into saying more! (...but we don't need the trigger phrase, next time they talk)
                 datestamp_of_last_goading = datetime.datetime.now()
                 demand_triggerphrase = False
@@ -158,25 +173,47 @@ def indefinitely_turn_text_into_commands(s2t, text_queue, triggerphrases, goadin
             elif cutoff_point >= 0 or demand_triggerphrase is False:
                 if cutoff_point >= 0:
                     command = sentence[cutoff_point:]
+                    print("The given command: >>{command}<<".format(command=command))
 #                    print(">>{sentence}<< Command found: >>{command}<<".format(sentence=sentence, command=command))
                 else:
                     command = sentence
-#                    print(">>{sentence}<< I couldn't find a trigger phrase. That doesn't matter, though: I was already primed for a command.".format(sentence=sentence))
+                    print("Follow-up command: >>{command}<<".format(command=command))
+                    print(">>{sentence}<< I couldn't find a trigger phrase. That doesn't matter, though: I was already primed for a command.".format(sentence=sentence))
                 s2t.mute = True
                 try:
                     process_command_func(command)
+                    s2t.adjust_mic_for_ambient_noise()  # FIXME: Try this!
                     demand_triggerphrase = True
                 except UnknownCommandError:
-                    print("Failed to execute >>{command}<< (unknown command).")
+                    print("Failed to execute >>{command}<< (unknown command).".format(command=command))
                     if demand_triggerphrase is False:
                         print("Just in case there's a less insane command on the way, I'll wait a little while longer before reinstituting my demand for a triggerphrase.")
-                sentence = ''
-                empty_queue(text_queue)
-                s2t.mute = False
+                except Exception as e:
+                    print("Warning --- exception occurred --- {e}".format(e=str(e)))
+                finally:
+                    sentence = ''
+                    empty_queue(text_queue)
+                    s2t.mute = False
             else:
                 print("Sentence >>{sentence}<< ...ignored (for now)".format(sentence=sentence))
                 time.sleep(.1)
 
+
+def handle_alarm_command(tts, text):
+    timetxt = None
+    for phr in ('up at', 'alarm for', 'alarm four', ''):
+        i = text.find(phr)
+        if i >= 0:
+            timetxt = ' ' + text[i + len(phr) + 1:] + ' '
+            timetxt = timetxt.replace(' for ', ' four ')  # in case of 'for forty' or 'ten oh for'
+            timetxt = timetxt.strip()
+            break
+    if timetxt is None:
+        print("Unable to deduce time text")
+    else:
+        print("Tryng to calculate hour and mins from time text >>{timetxt}<<".format(timetxt=timetxt))
+        hrs, mns = text2time(text.split(' at ')[1])
+        print("hrs =", hrs, "mns =", mns)
 
 
 def main():
@@ -197,8 +234,8 @@ def main():
     G_our_cached_weather_report_messages = SelfCachingCall(180, generate_weather_audio, tts, owner_name)
     G_stop = False
     s2t.always_adjust = True
-    s2t.max_recording_time = 3
-    s2t.pause_threshold = 0.5
+    s2t.max_recording_time = 5
+    s2t.pause_threshold = 0.8
     audio_queue = Queue()  # FIXME: Add a limit. Add exception-catching for when we accidentally overload the queue, too.
     text_queue = Queue()  # FIXME: Make thread-safe, if not already. See https://superfastpython.com/thread-queue/
     audio_thread = Thread(target=indefinitely_capture_snatches_of_audio_from_microphone, args=(s2t, audio_queue,))
@@ -212,6 +249,7 @@ def main():
                                          process_command_func=lambda txt: process_text(tts, txt))
     convertsounds_thread.join()
     audio_thread.join()
+
 
 
 if __name__ == '__main__':
