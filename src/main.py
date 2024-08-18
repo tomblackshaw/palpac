0f1a2fad6c86db35fa5ee03b9cadc01df49ef868
Tom Blackshaw <tom.blackshaw@protonmail.com>
2024-08-19 01:28:27 +0200

    more stuff

--- src/main.py
+++ src/main.py
@@ -22,6 +22,36 @@ Todo:
 .. _Style Guide:
    https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
 
+
+
+0 0
+1 51
+2 60
+3 68
+4 75
+5 81
+6 86
+7 90
+8 93
+9 95
+10 96
+11 100
+'''
+
+# from my.classes.text2speechclass import convert_audio_recordings_list_into_an_mp3_file
+# from my.stringutils import convert_24h_and_mins_to_shorttime
+
+# def speak_from_cache(voice, phrases):
+#     from my.text2speech import Text2SpeechSingleton as tts
+#     data = []
+#     tts.voice = voice
+#     for txt in phrases:
+#         data.append(get_cached_audio_for_phrase(tts.voice, txt))
+#     convert_audio_recordings_list_into_an_mp3_file(data, 'out.mp3', trim_level=1)
+#     os.system("mpv out.mp3")
+
+
+
 """
 
 import os
