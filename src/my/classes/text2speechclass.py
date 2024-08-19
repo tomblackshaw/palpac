346ada830f24d069b443dfaacf2768d829fa8f8d
Tom Blackshaw <tom.blackshaw@protonmail.com>
2024-08-19 22:54:38 +0200

    Added comments

--- src/my/classes/text2speechclass.py
+++ src/my/classes/text2speechclass.py
@@ -406,15 +406,24 @@ class _Text2SpeechClass:
 
 
 def detect_leading_silence(sound, silence_threshold=DEFAULT_SILENCE_THRESHOLD, chunk_size=10):
-    '''
-    sound is a pydub.AudioSegment
-    silence_threshold in dB
-    chunk_size in ms
+    """Detect the leading silence in a sound sample.
 
-    iterate over chunks until you find the first one with sound
-    '''
+    If there's silence at the start of the sound sample, find it. Iterate over\
+    chunks until you find the first one with sound
+
+    Args:
+        sound (AudioSegment): The audio data.
+        silence_threshold (float): The silence threshold, for 'squelching', in decibels.
+        chunk_size (int): How many milliseconds should we examine at a time.
+
+    Returns:
+        int: Number of milliseconds of leading silence that we found.
+
+    Raises:
+        Unknown.
+
+    """
     trim_ms = 0  # ms
-
     assert chunk_size > 0  # to avoid infinite loop
     while sound[trim_ms:trim_ms + chunk_size].dBFS < silence_threshold and trim_ms < len(sound):
         trim_ms += chunk_size
@@ -422,6 +431,28 @@ def detect_leading_silence(sound, silence_threshold=DEFAULT_SILENCE_THRESHOLD, c
 
 
 def convert_audio_recordings_list_into_one_audio_recording(data, trim_level=0):
+    """Convert a list of audio data into one AudioSegment instance.
+
+    Write the supplied list of data (probably MP3 to individual files.
+    Use the pydub library to combine them into a single AudioSegment
+    instance.
+
+    Args:
+        data (list[bytes()]): The list of MP3 data. I say 'list' because
+            this is a *list* of several bytes, not several *bytes*. To
+            access MP3 data #0, read data[0]. That's the first MP3 file.
+            The second is data[1]. You get the picture, I hope.
+        trim_level (int): If 0, don't trim. If 1, trim. If 2, trim aggressively.
+        exportfile (str): The pathname of the output file.
+
+    Returns:
+        AudioSegment: An instance of an AudioSegment, ready to be exported
+            etc. via the 'export' method: AudioSegment.export
+
+    Raises:
+        Unknown.
+
+    """
     sounds = None
     filenames = ''
     for d in data:
@@ -443,6 +474,22 @@ def convert_audio_recordings_list_into_one_audio_recording(data, trim_level=0):
 
 
 def trim_my_audio(untrimmed_audio, trim_level):
+    """Trim the suppiled audio.
+
+    If there's silence at the start and/or end of the sound sample, trim it off.
+
+    Args:
+        untrimmed_audio (AudioSegment): The untrimmed audio data.
+        trim_level (int): How much trimming should we do?
+            0=nearly none; 1=normal; 2=aggressive.
+
+    Returns:
+        AudioSegment: Trimmed audio.
+
+    Raises:
+        Unknown.
+
+    """
     silence_threshold = SNIPPY_SILENCE_THRESHOLD if trim_level > 1 else DEFAULT_SILENCE_THRESHOLD if trim_level == 0 else LAZY_SILENCE_THRESHOLD
     start_trim = detect_leading_silence(untrimmed_audio, silence_threshold=silence_threshold)
     end_trim = detect_leading_silence(untrimmed_audio.reverse(), silence_threshold=silence_threshold)
