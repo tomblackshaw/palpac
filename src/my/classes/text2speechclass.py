bc4d3b5590d6fdbfef6aa2a6b98f4342d7b9fdd9
Tom Blackshaw <tom.blackshaw@protonmail.com>
2024-08-19 11:56:12 +0200

    moar

--- src/my/classes/text2speechclass.py
+++ src/my/classes/text2speechclass.py
@@ -48,13 +48,11 @@ from random import choice
 import os
 
 from elevenlabs.client import ElevenLabs, Voice
-
-# from pydub import AudioSegment
 from pydub.audio_segment import AudioSegment
 
 from my.classes import singleton, ReadWriteLock
-from my.classes.exceptions import MissingVoskAPIKeyError, ElevenLabsDownError
-from my.globals import ELEVENLABS_KEY_BASENAME
+from my.classes.exceptions import MissingVoskAPIKeyError, ElevenLabsDownError, MissingFromCacheError
+from my.globals import ELEVENLABS_KEY_BASENAME, DEFAULT_SILENCE_THRESHOLD, SNIPPY_SILENCE_THRESHOLD, LAZY_SILENCE_THRESHOLD
 from my.stringutils import flatten, generate_random_string
 
 
@@ -86,73 +84,6 @@ def get_elevenlabs_clientclass(key_filename):
         api_key=api_key)
     return client
 
-
-def detect_leading_silence(sound, silence_threshold=DEFAULT_SILENCE_THRESHOLD, chunk_size=10):
-    '''
-    sound is a pydub.AudioSegment
-    silence_threshold in dB
-    chunk_size in ms
-
-    iterate over chunks until you find the first one with sound
-    '''
-    trim_ms = 0  # ms
-
-    assert chunk_size > 0  # to avoid infinite loop
-    while sound[trim_ms:trim_ms + chunk_size].dBFS < silence_threshold and trim_ms < len(sound):
-        trim_ms += chunk_size
-
-    return trim_ms
-
-
-
-def convert_audio_recordings_list_into_an_mp3_file(data, exportfile, trim_level=0):
-    """Convert a list of audio data into an MP3 file.
-
-    Write the supplied list of data (probably MP3 to individual files.
-    Use the pydub library to combine them into a single MP3. Save it
-    to the specified pathname.
-
-    Args:
-        data (list[bytes()]): The list of MP3 data. I say 'list' because
-            this is a *list* of several bytes, not several *bytes*. To
-            access MP3 data #0, read data[0]. That's the first MP3 file.
-            The second is data[1]. You get the picture, I hope.
-        trim_level (int): If 0, don't trim. If 1, trim. If 2, trim aggressively.
-        exportfile (str): The pathname of the output file.
-
-    Returns:
-        n/a
-
-    Raises:
-        Unknown.
-
-    """
-    sounds = None
-    filenames = ''
-    for d in data:
-        fname = '/tmp/{rnd}'.format(rnd=generate_random_string(32))
-        filenames += ' {fname}'.format(fname=fname)
-        with open(fname, 'wb') as f:
-            f.write(d)
-        untrimmed_audio = AudioSegment.from_mp3(fname)
-        trimmed_aud = untrimmed_audio if trim_level == 0 else trim_my_audio(untrimmed_audio, trim_level)
-        if sounds is None:
-            sounds = trimmed_aud
-        else:
-            sounds += trimmed_aud
-    sounds.export(exportfile, format='mp3')
-    for fname in filenames.strip(' ').split(' '):
-        assert(fname[0] == '/')
-        os.unlink(fname)
-
-
-def trim_my_audio(untrimmed_audio, trim_level):
-    silence_threshold = SNIPPY_SILENCE_THRESHOLD if trim_level > 1 else DEFAULT_SILENCE_THRESHOLD
-    start_trim = detect_leading_silence(untrimmed_audio, silence_threshold=silence_threshold)
-    end_trim = detect_leading_silence(untrimmed_audio.reverse(), silence_threshold=silence_threshold)
-    duration = len(untrimmed_audio)
-    trimmed_aud = untrimmed_audio[start_trim:duration - end_trim]
-    return trimmed_aud
 
 
 @singleton
@@ -472,4 +403,76 @@ class _Text2SpeechClass:
                   }
                 )
             self.stream(audio_stream)
+
+
+def detect_leading_silence(sound, silence_threshold=DEFAULT_SILENCE_THRESHOLD, chunk_size=10):
+    '''
+    sound is a pydub.AudioSegment
+    silence_threshold in dB
+    chunk_size in ms
+
+    iterate over chunks until you find the first one with sound
+    '''
+    trim_ms = 0  # ms
+
+    assert chunk_size > 0  # to avoid infinite loop
+    while sound[trim_ms:trim_ms + chunk_size].dBFS < silence_threshold and trim_ms < len(sound):
+        trim_ms += chunk_size
+    return trim_ms
+
+
+def convert_audio_recordings_list_into_one_audio_recording(data, trim_level=0):
+    sounds = None
+    filenames = ''
+    for d in data:
+        fname = '/tmp/{rnd}'.format(rnd=generate_random_string(32))
+        filenames += ' {fname}'.format(fname=fname)
+        with open(fname, 'wb') as f:
+            f.write(d)
+        untrimmed_audio = AudioSegment.from_mp3(fname)
+        trimmed_aud = trim_my_audio(untrimmed_audio, trim_level)
+        if sounds is None:
+            sounds = trimmed_aud
+        else:
+            sounds += trimmed_aud
+    if sounds is not None:
+        for fname in filenames.strip(' ').split(' '):
+            assert(fname[0] == '/')
+            os.unlink(fname)
+    return sounds
+
+
+def trim_my_audio(untrimmed_audio, trim_level):
+    silence_threshold = SNIPPY_SILENCE_THRESHOLD if trim_level > 1 else DEFAULT_SILENCE_THRESHOLD if trim_level == 0 else LAZY_SILENCE_THRESHOLD
+    start_trim = detect_leading_silence(untrimmed_audio, silence_threshold=silence_threshold)
+    end_trim = detect_leading_silence(untrimmed_audio.reverse(), silence_threshold=silence_threshold)
+    duration = len(untrimmed_audio)
+    trimmed_aud = untrimmed_audio[start_trim:duration - end_trim]
+    return trimmed_aud
+
+
+def convert_audio_recordings_list_into_an_mp3_file(data, exportfile, trim_level=0):
+    """Convert a list of audio data into an MP3 file.
+
+    Write the supplied list of data (probably MP3 to individual files.
+    Use the pydub library to combine them into a single MP3. Save it
+    to the specified pathname.
+
+    Args:
+        data (list[bytes()]): The list of MP3 data. I say 'list' because
+            this is a *list* of several bytes, not several *bytes*. To
+            access MP3 data #0, read data[0]. That's the first MP3 file.
+            The second is data[1]. You get the picture, I hope.
+        trim_level (int): If 0, don't trim. If 1, trim. If 2, trim aggressively.
+        exportfile (str): The pathname of the output file.
+
+    Returns:
+        n/a
+
+    Raises:
+        Unknown.
+
+    """
+    sounds = convert_audio_recordings_list_into_one_audio_recording(data=data, trim_level=trim_level)
+    sounds.export(exportfile, format='mp3')
 
