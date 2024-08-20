29ae7dd7bb794ed181f93c65e600a7a27f4594e2
Tom Blackshaw <tom.blackshaw@protonmail.com>
2024-08-20 06:28:20 +0200

    generate_detokenized_message (not generate_alarm_message) now

--- src/test/text2speech.py
+++ src/test/text2speech.py
@@ -11,7 +11,7 @@ text2speech test
 import random
 import unittest
 
-from my.stringutils import generate_alarm_message
+from my.stringutils import generate_detokenized_message
 from my.text2speech import Text2SpeechSingleton as tts
 
 
@@ -24,27 +24,27 @@ class TestOne(unittest.TestCase):
         pass
 
     def testGoofy(self):
-        self.assertRaises(TypeError, generate_alarm_message, None, None, None, None, None, None)
-        self.assertRaises(TypeError, generate_alarm_message, None, None, None, None, None)
-        self.assertRaises(ValueError, generate_alarm_message, None, None, None, None)
-        self.assertRaises(TypeError, generate_alarm_message, None, None, None)
-        self.assertRaises(TypeError, generate_alarm_message, None, None)
-        self.assertRaises(TypeError, generate_alarm_message, None)
-        self.assertRaises(TypeError, generate_alarm_message)
-        self.assertRaises(TypeError, generate_alarm_message, '')
-        self.assertRaises(ValueError, generate_alarm_message, 'Biggles', -1, 0, 'Simple template')
-        self.assertRaises(ValueError, generate_alarm_message, 'Biggles', 'minus one', 'zero', 'Simple template')
-        self.assertRaises(ValueError, generate_alarm_message, 'Biggles', 0, 99, 'Simple template')
-        self.assertRaises(ValueError, generate_alarm_message, 'Biggles', 24, 0, 'Simple template')
-        self.assertRaises(ValueError, generate_alarm_message, 'Biggles', 0, 60, 'Simple template')
-        self.assertRaises(ValueError, generate_alarm_message, 'Biggles', 24, 60, 'Simple template')
+        self.assertRaises(TypeError, generate_detokenized_message, None, None, None, None, None, None)
+        self.assertRaises(TypeError, generate_detokenized_message, None, None, None, None, None)
+        self.assertRaises(ValueError, generate_detokenized_message, None, None, None, None)
+        self.assertRaises(TypeError, generate_detokenized_message, None, None, None)
+        self.assertRaises(TypeError, generate_detokenized_message, None, None)
+        self.assertRaises(TypeError, generate_detokenized_message, None)
+        self.assertRaises(TypeError, generate_detokenized_message)
+        self.assertRaises(TypeError, generate_detokenized_message, '')
+        self.assertRaises(ValueError, generate_detokenized_message, 'Biggles', -1, 0, 'Simple template')
+        self.assertRaises(ValueError, generate_detokenized_message, 'Biggles', 'minus one', 'zero', 'Simple template')
+        self.assertRaises(ValueError, generate_detokenized_message, 'Biggles', 0, 99, 'Simple template')
+        self.assertRaises(ValueError, generate_detokenized_message, 'Biggles', 24, 0, 'Simple template')
+        self.assertRaises(ValueError, generate_detokenized_message, 'Biggles', 0, 60, 'Simple template')
+        self.assertRaises(ValueError, generate_detokenized_message, 'Biggles', 24, 60, 'Simple template')
 
     def testThis(self):
         for owner_name in ('Bill', 'Ted', 'Excellent', 'Adventure'):
             for time_24h in range(0, 24):
                 for time_minutes in range(0, 60):
-                    _ = generate_alarm_message(owner_name, time_24h, time_minutes, 'Simple template ${shorttime} ${owner} yep yep')
-                    self.assertRaises(KeyError, generate_alarm_message, owner_name, time_24h, time_minutes, 'Simple ${wtf_is_this} template')
+                    _ = generate_detokenized_message(owner_name, time_24h, time_minutes, 'Simple template ${shorttime} ${owner} yep yep')
+                    self.assertRaises(KeyError, generate_detokenized_message, owner_name, time_24h, time_minutes, 'Simple ${wtf_is_this} template')
 
     def testFieldAdvanced(self):
         for i in (True, False, True, True, False, False, True, False):
@@ -134,6 +134,15 @@ class TestOne(unittest.TestCase):
 
         self.assertRaises(ValueError, myfuncI)
         self.assertRaises(TypeError, myfuncV)
+
+
+class TestTwo(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
 
 
 if __name__ == "__main__":
