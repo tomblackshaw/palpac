c0a4b38f0616abe77ed7dc7a91d4bafcfa88f986
Tom Blackshaw <tom.blackshaw@protonmail.com>
2024-09-22 21:52:37 +0200

    Tidied up fart.py

--- src/fart.py
+++ src/fart.py
@@ -2,20 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import random
+import os
+from os.path import join, isdir
+from os import listdir
 
-voices_lst = ['Daniel', 'Chris', 'Jake- Smart, Formal, Confident', 'Sarah',
-                  'New York Nick - Modern NYC Wiseguy', 'Eric', 'Alice', 'Callum',
-                  'Olivier Calm', 'Liam', 'Laura', 'Jake - Smart, Formal, Confident',
-                   'Will', 'Natasha - Valley girl', 'Sonia', 'Charlotte', 'Jessica',
-                   'Frederick - Old Gnarly Narrator', 'George', 'Matilda',
-                    'Charlie', 'Bill', 'Brian', 'Maya', 'Lily', 'Hugo']
+
 if __name__ == '__main__':
     from my.text2speech import fart_and_apologize
-    if len(sys.argv) == 1 or sys.argv[1] not in voices_lst:
-        print("Options:", voices_lst)
-        sys.exit(1)
-
-    fart_and_apologize(sys.argv[1])
+    path = 'audio/cache'
+    voices_lst = [f for f in listdir(path) if isdir(join(path, f))]
+    voice = random.choice(voices_lst)
+    fart_and_apologize(voice)
 
 
 
