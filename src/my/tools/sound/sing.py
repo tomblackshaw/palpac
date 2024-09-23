69d64e51eb1e0fba05d7aa49b93bfeb5b1c29e13
Tom Blackshaw <tom.blackshaw@protonmail.com>
2024-09-23 20:21:07 +0200

    numpy import shouldn't break things now

--- src/my/tools/sound/sing.py
+++ src/my/tools/sound/sing.py
@@ -41,13 +41,13 @@ from pydub.audio_segment import AudioSegment
 try:
     import librosa
     import psola
+    import numpy as np
 except ImportError as e:
     print("Warning -- I can't sing. Such infernal purposes...")
     librosa = None  # hack, to silence the main subroutine. We don't want to sing anyway: it's not very good when we do.
 
 from my.stringutils import generate_random_string
 from my.tools.sound.trim import convert_audio_recordings_list_into_an_mp3_file
-import numpy as np
 import soundfile as sf
 
 
