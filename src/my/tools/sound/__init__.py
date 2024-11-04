3b6ac212ab37c033ab2003e7326037a882351107
Tom Blackshaw <tom.blackshaw@protonmail.com>
2024-11-04 11:17:23 +0100

    Removed weather code

--- src/my/tools/sound/__init__.py
+++ src/my/tools/sound/__init__.py
@@ -1,6 +1,37 @@
-'''
-'''
+# -*- coding: utf-8 -*-
+"""Example Google style docstrings.
 
+This module demonstrates documentation as specified by the `Google Python
+Style Guide`_. Docstrings may extend over multiple lines. Sections are created
+with a section header and a colon followed by a block of indented text.
+
+Example:
+    Examples can be given using either the ``Example`` or ``Examples``
+    sections. Sections support any reStructuredText formatting, including
+    literal blocks::
+
+        $ python example_google.py
+
+Section breaks are created by resuming unindented text. Section breaks
+are also implicitly created anytime a new section starts.
+
+Attributes:
+    module_level_variable1 (int): Module level variables may be documented in
+        either the ``Attributes`` section of the module docstring, or in an
+        inline docstring immediately following the variable.
+
+        Either form is acceptable, but the two should not be mixed. Choose
+        one convention to document module level variables and be consistent
+        with it.
+
+Todo:
+    * For module TODOs
+    * You have to also use ``sphinx.ext.todo`` extension
+
+.. _Google Python Style Guide:
+   http://google.github.io/styleguide/pyguide.html
+
+"""
 
 import pygame  # @UnresolvedImport
 import time
@@ -12,9 +43,10 @@ import os
 from os import listdir
 from os.path import isfile, join
 from my.classes.exceptions import MissingFromCacheError
+from threading import Thread
+from queue import Empty, Queue
+ogg_queue = Queue()
 
-
-pygame.mixer.init()
 
 def stop_sounds():
     pygame.mixer.music.stop()
@@ -82,7 +114,7 @@ def convert_one_mp3_to_ogg_file(mp3fname, oggfname):
     assert(os.path.exists(mp3fname))
     try:
         os.unlink(oggfname)
-    except:
+    except FileNotFoundError as _:
         pass
     untrimmed_audio = AudioSegment.from_mp3(mp3fname)
     trimmed_aud = trim_my_audio(untrimmed_audio, trim_level=1)
@@ -105,17 +137,6 @@ def ogg_file_queue_thread_func(qu):
                 print("ogg_file_queue_thread_func() -- cannot play %s: it doesn't exist" % item)
             qu.task_done()
 
-from threading import Thread
-
-from queue import Empty, Queue
-ogg_queue = Queue()
-
-consumer_thread = Thread(
-        target=ogg_file_queue_thread_func,
-        args=(ogg_queue,),
-        daemon=True
-    )
-consumer_thread.start()
 
 
 
@@ -125,3 +146,19 @@ def queue_oggfile(fname):
     if not os.path.exists(fname):
         raise MissingFromCacheError("queue_oggfile() cannot queue %s: it doesn't exist" % fname)
     ogg_queue.put(fname)
+
+
+
+
+
+
+consumer_thread = Thread(
+        target=ogg_file_queue_thread_func,
+        args=(ogg_queue,),
+        daemon=True
+    )
+consumer_thread.start()
+
+
+pygame.mixer.init()
+
